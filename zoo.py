f5b0a383ac9abfa05d7ab39e7ff1e6c9e9998926
Lo Nadine <nadine.lo.2022@ensta-paris.fr>
2020-04-06 21:17:05 +0200

    Ajout d'un main

--- zoo.py
+++ zoo.py
@@ -11,14 +11,14 @@ class Giraffe(Animal):
         self.food=food
         self.healthcare=healthcare
 
-    def get_height_neck(self):
+    def get_height(self):
         return self.height
     
     def get_daily_routine(self):
-        print("You need to give them" self.food "and" self.healthcare)
+        print("You need to give them ", self.food," and ", self.healthcare)
         return(0)      
 
-  class Elephant(Animal):
+class Elephant(Animal):
     def __init__(self, age,sex, weight, name, species, origin):
         Animal.__init__(self,age,sex,weight)
         self.name=name
@@ -27,9 +27,17 @@ class Giraffe(Animal):
         
         
     def presentation(self):
-        print ("I am" self.name "and I am a" self.species "elephant from" self.origin)
+        print ("I am ", self.name, " and I am a ", self.species, " elephant from", self.origin)
         return (0)
-    
-    def tusks(self)
+
+    def tusks(self):
         print("I have two tusks!")
         return(0)
+
+if __name__=="__main__":
+    Animal1=Giraffe(13,"female",120,5.2,"acacia","vaccine")
+    Animal2=Elephant(12,"male",300,"Dumbo","African","Congo")
+    print("What Animal1 need today ?")
+    print(Animal1.get_daily_routine())
+    print("What is Animal2 ?")
+    print(Animal2.presentation())
