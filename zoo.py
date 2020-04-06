e418e984475ba3ad0e1fc3b1d611df0b3f54c7a9
lena-abalo <63007349+lena-abalo@users.noreply.github.com>
2020-04-06 19:11:00 +0200

    Update zoo.py
    Ajout de Lena

--- zoo.py
+++ zoo.py
@@ -18,5 +18,18 @@ class Giraffe(Animal):
         print("You need to give them" self.food "and" self.healthcare)
         return(0)      
 
-
-
+  class Elephant(Animal):
+    def __init__(self, age,sex, weight, name, species, origin):
+        Animal.__init__(self,age,sex,weight)
+        self.name=name
+        self.species=species
+        self.origin=origin
+        
+        
+    def presentation(self):
+        print ("I am" self.name "and I am a" self.species "elephant from" self.origin)
+        return (0)
+    
+    def tusks(self)
+        print("I have two tusks!")
+        return(0)
