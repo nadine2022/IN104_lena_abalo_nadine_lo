69a1f4601954cc999410f530a19734ae6308b886
nadine2022 <62844903+nadine2022@users.noreply.github.com>
2020-04-10 14:13:20 +0200

    Update zoo.py

--- zoo.py
+++ zoo.py
@@ -5,18 +5,24 @@ class Animal:
         self.weight=weight
 
 class Giraffe(Animal):
-    def __init__(self, age,sex, weight, height,food,healthcare):
+    def __init__(self, age,sex, weight, height,food,health):
         Animal.__init__(self,age,sex,weight)
         self.height=height
-        self.food=food
-        self.healthcare=healthcare
+        self.food=food #score between 0 and 100 (typ int)
+        self.healthcare=healthcare #score between 0 and 10 (type float)
 
     def get_height(self):
         return self.height
     
-    def get_daily_routine(self):
-        print("You need to give them ", self.food," and ", self.healthcare)
-        return(0)      
+    def get_daily_check(self):
+        if self.health<5.0:
+            print("Call the vet immediately")
+        if self.food<50 :
+            print("Give him some supplements")
+        else:
+            if self.food<75:
+                if self.weight<200: #above the animal should do a diet
+                    print("Give him some supplements")
 
 class Elephant(Animal):
     def __init__(self, age,sex, weight, name, species, origin):
@@ -35,9 +41,9 @@ class Elephant(Animal):
         return(0)
 
 if __name__=="__main__":
-    Animal1=Giraffe(13,"female",120,5.2,"acacia","vaccine")
+    Animal1=Giraffe(13,"female",120,5.2,70,4.0)
     Animal2=Elephant(12,"male",300,"Dumbo","African","Congo")
     print("What Animal1 need today ?")
-    print(Animal1.get_daily_routine())
+    print(Animal1.get_daily_check())
     print("What is Animal2 ?")
     print(Animal2.presentation())
