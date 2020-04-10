a8e49d236d9511df3579f583ea41f12b242fd1f4
nadine2022 <62844903+nadine2022@users.noreply.github.com>
2020-04-10 15:20:26 +0200

    ne marche pas

--- TDD/unittests_nadine_lo.py
+++ TDD/unittests_nadine_lo.py
@@ -1,1 +1,8 @@
+import unittest
+import zoo
 
+class BadInput(unittest.TestCase):
+
+    def test_species(self):
+        """the species are Asian or African"""
+        self.assertRaises(zoo.InvalidSyntax,zoo.Elephant.presentation,"European")
