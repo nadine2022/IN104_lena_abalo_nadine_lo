98e13d981e099fb51b9709142a59e7d725349eb4
nermine-ali <61667536+nermine-ali@users.noreply.github.com>
2020-04-13 18:49:39 +0200

    code reformatting and improvements

--- TDD/zoo1.py
+++ TDD/zoo1.py
@@ -1,49 +1,57 @@
-#zoo1.py
-class InvalidSyntax(ValueError) : pass
-class InvalidData(ValueError) : pass
+class InvalidSyntax(ValueError):
+    pass
+
+
+class InvalidData(ValueError):
+    pass
+
 
 class Animal:
-    def __init__(self,age,sex,weight):
-        self.age=age
-        self.sex=sex
-        self.weight=weight
+    def __init__(self, age, sex, weight):
+        self._age = age
+        self.sex = sex
+        self.weight = weight
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if self._age < 0:
+            raise Exception("age cannot be negative.")
+        self._age = value
 
 
 class Elephant(Animal):
-    def __init__(self, age,sex, weight, name, species, origin, children):
-        Animal.__init__(self,age,sex,weight)
-        self.name=name
-        self.species=species
-        self.origin=origin
-        self.children = children #a list with the names of the children
-        
-    def new_birth(self,name):
+    def __init__(self, age, sex, weight, name, species, origin, children):
+        Animal.__init__(self, age, sex, weight)
+        self.name = name
+        self.species = species
+        self.origin = origin
+        self.children = children  # a list with the names of the children
 
-        if isinstance(name,str)==False:
+    def new_birth(self, name):
+        if not isinstance(name, str):
             raise InvalidSyntax("the name should be a string")
-        if name=="":
-            self.children=[]
+        if name == "":
+            self.children = []
         else:
             self.children = self.children + [name]
-        #print (self.name, "gave birth to", name)
-        #return (0)
 
-    def get_children(self):
-        return self.children
-        
-        
     def presentation(self):
-
-        if self.species not in ("Asian","African"):
+        if self.species not in ("Asian", "African"):
             raise InvalidData("Just two species exist : Asian and African")
 
-        if (self.age<0) or (isinstance(self.age,int)==False):
+        if (self.age < 0) or not (isinstance(self.age, int)):
             raise InvalidData("age is a positive int")
-        
-        print ("I am ", self.name, " and I am a ", self.species, " elephant from", self.origin)
-        self.age+=1
-        print("Today is my birthday, so I am",self.age)
-        return (0)
+
+        print("I am ", self.name, " and I am a ", self.species, " elephant from", self.origin)
+        self.age += 1
+        print("Today is my birthday, so I am", self.age)
+        return 0
 
 
-
+if __name__ == '__main__':
+    el = Elephant(12, "male", 300, "Dumbo", "European", "Congo", ["Simba", "Coco"])
+    print(el.age)
--- zoo.py
+++ zoo.py
@@ -1,63 +1,69 @@
 class OutOfRangeError(ValueError):
     pass
-class Errors : pass
-class InvalidSyntax(Errors):pass
+
+
+class Errors: pass
+
+
+class InvalidSyntax(Errors): pass
+
 
 class Animal:
-    def __init__(self,age,sex,weight):
-        self.age=age
-        self.sex=sex
-        self.weight=weight
+    def __init__(self, age, sex, weight):
+        self.age = age
+        self.sex = sex
+        self.weight = weight
+
 
 class Giraffe(Animal):
-    def __init__(self, age,sex, weight, height,food,healthcare):
-        Animal.__init__(self,age,sex,weight)
-        self.height=height
-        self.food=food #score between 0 and 100 (typ int)
-        self.healthcare=healthcare #score between 0 and 10 (type float)
+    def __init__(self, age, sex, weight, height, food, healthcare):
+        Animal.__init__(self, age, sex, weight)
+        self.height = height
+        self.food = food  # score between 0 and 100 (typ int)
+        self.healthcare = healthcare  # score between 0 and 10 (type float)
 
     def get_height(self):
         return self.height
-    
+
     def add_food(self, foo):
         self.food += foo
         return self.food
-    
+
     def get_daily_check(self):
-        if self.healthcare<5.0:
+        if self.healthcare < 5.0:
             print("Call the vet immediately")
-        if self.food<50 :
+        if self.food < 50:
             print("Give him some supplements")
         else:
-            if self.food<75:
-                if self.weight<200: #above the animal should do a diet
+            if self.food < 75:
+                if self.weight < 200:  # above the animal should do a diet
                     print("Give him some supplements")
 
+
 class Elephant(Animal):
-    def __init__(self, age,sex, weight, name, species, origin, children):
-        Animal.__init__(self,age,sex,weight)
-        self.name=name
-        self.species=species
-        self.origin=origin
-        self.children = children #a list with the names of the children
-        
-    def new_birth(self,name):
+    def __init__(self, age, sex, weight, name, species, origin, children):
+        Animal.__init__(self, age, sex, weight)
+        self.name = name
+        self.species = species
+        self.origin = origin
+        self.children = children  # a list with the names of the children
+
+    def new_birth(self, name):
         self.children = self.children + [name]
-        print (self.name, "gave birth to", name)
+        print(self.name, "gave birth to", name)
         return (0)
 
     def get_children(self):
         return self.children
-        
-        
+
     def presentation(self):
-        print ("I am ", self.name, " and I am a ", self.species, " elephant from", self.origin)
+        print("I am ", self.name, " and I am a ", self.species, " elephant from", self.origin)
         return (0)
 
 
-if __name__=="__main__":
-    Animal1=Giraffe(13,"female",120,5.2,70,4.0)
-    Animal2=Elephant(12,"male",300,"Dumbo","African","Congo")
+if __name__ == "__main__":
+    Animal1 = Giraffe(13, "female", 120, 5.2, 70, 4.0)
+    Animal2 = Elephant(12, "male", 300, "Dumbo", "African", "Congo", "ch")
     print("What Animal1 need today ?")
     print(Animal1.get_daily_check())
     print("What is Animal2 ?")
