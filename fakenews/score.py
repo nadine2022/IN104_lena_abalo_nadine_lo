16c264f430c83bea8d6b2fcd7a997641d99b9ae2
Lo Nadine <nadine.lo.2022@ensta-paris.fr>
2020-05-10 14:31:09 +0200

    3 mots au lieu de 2

--- fakenews/score.py
+++ fakenews/score.py
@@ -4,19 +4,21 @@ from mots import selection
 
 def give_score():
     
-    mots_clés=selection()
+    mots_clés=selection()[:]
+    mots_clés1=mots_clés[:] #liste intermédiaire
     mots_indic = [] #On veut ici récupérer les deux mots-clés ayant le moins d'apparitions dans les articles, 
-    i=0
-    while i<2:     #On réalise deux fois l'opération afin d'avoir les deux mots
+    j=0
+    while j<3:     #On réalise deux fois l'opération afin d'avoir les deux mots
         indexe_indic = 0
-        indic = nombre_articles(mots_clés[0]) 
-        for i in range (1, len(mots_clés)):
-        if ((nombre_articles(mots_clés[i]) <indic) and (nombre_articles(mots_clés[i])!=0)):
-            indic = nombre_articles(mots_clés[i])
-            indexe_indic = i
-        mots_indic.append(mots_clés[index_indic])
-        del mots_clés[indexe_indic]  #On retire le mot-clé déjà récupéré de la liste des mots-clés
-        i+=1
+        indic = nombre_articles(mots_clés1[0]) 
+        for i in range (1, len(mots_clés1)):
+            if ((nombre_articles(mots_clés1[i]) <indic) and (nombre_articles(mots_clés1[i])!=0)):
+                indic = nombre_articles(mots_clés1[i])
+                indexe_indic = i
+        mots_indic.append(mots_clés1[indexe_indic])
+        del mots_clés1[indexe_indic]  #On retire le mot-clé déjà récupéré de la liste des mots-clés
+        j+=1
+    print(mots_indic)
     
     nombre_indic = mots_clés_à_chercher(mots_indic) #Notre indicateur ici est le nombre d'articles comportant les deux mots avec le moins d'apparitions
     nombre_total = mots_clés_à_chercher(mots_clés) #On récupère le nombre d'articles qui contiennent tous nos mots clés
