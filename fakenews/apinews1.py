6b7ffe6773a0499e20b5739fc9303949940bf619
Lo Nadine <nadine.lo.2022@ensta-paris.fr>
2020-05-18 22:06:54 +0200

    Restriction des sources

--- fakenews/apinews1.py
+++ fakenews/apinews1.py
@@ -7,7 +7,7 @@ newsapi=NewsApiClient(api_key='cc48bcc39628400db2715d790b287b32')
 def nombre_articles(mots_cles):
 
     data = newsapi.get_everything(q=mots_cles,sources='abc-news,bbc-news,bbc-sport,bleacher-report,business-insider,business-insider-uk,cbs-news,cnbc,cnn,engadget,espn,independent,mashable,national-geographic,nbc-news,new-scientist,newsweek,new-york-magazine,reuters,the-verge,the-wall-street-journal,the-washington-post,the washington-times,time,usa-today', language='en')
-    return(data['totalResults'])
+    return(len(data['articles']))
     
 
 
@@ -17,10 +17,11 @@ def base_articles(mots_cles):
     #construction de la base de donnée
     n=len(data)
 
-    for i in range(0,n):
-        Article(source=data['articles'][i]['source']['name'],
-                title=data['articles'][i]['title'],
-                pub_date=data['articles'][i]['publishedAt']).save()
+    #for i in range(0,n):
+        
+       #Article(source=data['articles'][i]['source']['name'],
+                #title=data['articles'][i]['title'],
+                #pub_date=data['articles'][i]['publishedAt']) #.save()
 
-    return Article.objects.all() #la base de données
+    return data  #la base de données
 
