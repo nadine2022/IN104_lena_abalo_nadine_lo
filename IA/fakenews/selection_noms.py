d1eadec4943b193846971273fa491cee9e7400f7
lena-abalo <63007349+lena-abalo@users.noreply.github.com>
2020-05-20 00:07:18 +0200

    Update selection_noms.py

--- IA/fakenews/selection_noms.py
+++ IA/fakenews/selection_noms.py
@@ -6,15 +6,16 @@ def selection_noms(): #Fonction qui sélectionne les mots importants
     txt=input('News : ')
     doc = nlp(txt)
     mots=[token.text for token in doc]
+    nat = [token.pos_ for token in doc]
 
     from spacy.lang.en.stop_words import STOP_WORDS
  
     mots_filtres =''
-
-    for x in mots:
-        lex = nlp.vocab[x]
-        if lex.is_stop == False:  #On ne considère que les mots qui ne sont pas des stopswords(adjectfs, adverbes, pronoms...)                 
-            mots_filtres+=x+' ' 
+    n=len(mots)
+    for i in range(0,n):
+        lex = nlp.vocab[mots[i]]
+        if lex.is_stop == False and (nat[i] != 'VERB') and (nat[i] != 'AUX'):  #On ne considère que les mots qui ne sont ni des stopswords(adjectfs, adverbes, pronoms...), ni des verbes                 
+            mots_filtres+=mots[i]+' ' 
  
     print(mots_filtres)   
 
