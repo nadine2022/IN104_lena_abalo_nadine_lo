fbc19cdd521d0d76cdba15365a02253573dd8978
lena-abalo <63007349+lena-abalo@users.noreply.github.com>
2020-05-19 23:07:43 +0200

    Create selection_verbes.py

---
+++ IA/fakenews/selection_verbes.py
@@ -0,0 +1,21 @@
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+def selection_verbes(): #fonction qui séléctionne les verbes de la phrase pour la 2e sélection
+    txt=input('News : ')
+    doc = nlp(txt)
+    
+    verbs=''
+    mots=[token.text for token in doc]
+    nat = [token.pos_ for token in doc] #On récupère les natures de tous les mots
+    
+    n = len(nat)
+    for i in range (0,n):
+        if nat[i] =='VERB' or nat[i]=='AUX': #On prend uniquement les verbes et auxiliaires
+            verbs+= mots[i]+' '
+    
+    doc1=nlp(verbs)  #On crée un doc avec les verbes afin de pouvoir les mettre à l'infinitif
+    verb_list=[token.lemma_ for token in doc1]
+    print (verb_list)  
+
+selection_verbes()
