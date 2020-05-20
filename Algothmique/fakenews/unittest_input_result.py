f972552cef3769e48953c1b38f063d7c39a2cbc4
lena-abalo <63007349+lena-abalo@users.noreply.github.com>
2020-05-20 22:02:49 +0200

    Update unittest_input_result.py

--- Algothmique/fakenews/unittest_input_result.py
+++ Algothmique/fakenews/unittest_input_result.py
@@ -1,41 +1,41 @@
 import score 
 import unittest
-from score import scores, OutOfRangeError
+from score import give_score, OutOfRangeError
 
 class KnownValues(unittest.TestCase):
     known_values = (
         (
-            "The Olympic Games in Tokyo has been postponed",
-            49.275
+            "US counts more than one milion coronavirus cases",
+            40
         ),
         (
-            "Scientists believe that the earth is flat",
-            6.5
+            "France won the last World Cup",
+            65
         ),
         (
-            "Donald Trump is taking somme chloroquine",
-            85.975
+            "Donald Trump is the US president",
+            52
         ),
         
     )
 
     def test_score_known_values(self):
-        """score should give known result with known input"""
+        """give_score should give known result with known input"""
         for phrase, known in self.known_values:
-            self.assertEqual(known, scores(phrase))
+            self.assertEqual(known, give_score(phrase))
 
 
 class ScoreBadInput(unittest.TestCase):
 
     def test_is_empty(self):
-        """score should fail with an empty input"""
+        """give_score should fail with an empty input"""
         phrase=""
-        self.assertRaises(OutOfRangeError, scores, phrase)
+        self.assertRaises(OutOfRangeError, give_score, phrase)
 
     def test_number_words(self):
-        """score should fail with a too short sentence"""
+        """give_score should fail with a too short sentence"""
         phrase="Macron is"
-        self.assertRaises(OutOfRangeError, scores, phrase)
+        self.assertRaises(OutOfRangeError, give_score, phrase)
         
  if __name__ == "__main__":
     unittest.main()
